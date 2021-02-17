b70add2059bc8255cc9f33a0d9b509937aeff8e1
Patrick <patrick.yrkim@gmail.com>
2021-02-16 23:46:26 -0800

    Add create pages

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,22 +1,142 @@
-import React from "react"
-import { Link } from "gatsby"
-
+import React from "react";
 import Layout from "../components/layout"
-import Image from "../components/image"
-import SEO from "../components/seo"
+import { graphql } from "gatsby"
+// import { Link, graphql } from "gatsby"
+// import { Carousel } from 'react-bootstrap'
+import Preview from "../components/preview"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
+const IndexPage = ({ data }) => {
+  // const sortedByDate = this.props.data.allStrapiArticle.edges.sort((a, b) => {
+  const sortedByDate = data.allStrapiArticle.edges.sort((a, b) => {
+    let aDate = parseInt(a.node.published_at.split("T")[0].split("-").join(""))
+    let bDate = parseInt(b.node.published_at.split("T")[0].split("-").join(""))
+    return (bDate - aDate)
+  });
 
-export default IndexPage
+  const recentArticles = sortedByDate.slice(0, 3);
+
+  const labscopeArticles = sortedByDate.filter(document => (
+    // document.node.categories.map(cat => cat.title === 'Labscope')
+    document.node.categories.map(cat => cat.title)[0] === 'Labscope'
+    // document.node.categories.map(cat => cat.title).includes(‘Labscope’)
+    // document.node.categories[0].title === 'Labscope'
+  )).slice(0, 5);
+
+  const noteworthyArticles = sortedByDate.filter(document => (
+    // document.node.categories.map(cat => cat.title === 'Noteworthy News')
+    document.node.categories.map(cat => cat.title)[0] === 'Noteworthy News'
+    // document.node.categories.map(cat => cat.title).includes('Noteworthy News')
+    // document.node.categories[0].title === 'Noteworthy News'
+  )).slice(0, 5);
+
+  return (
+    <Layout>
+      <div>
+        <div id="carousel">
+          {/* <Carousel>
+            {recentArticles.map(document => (
+              <Carousel.Item>
+                {document.node.image ? <img src={document.node.image.publicURL} className="object-cover w-20 h-20" alt="" /> : ""}
+                <Carousel.Caption>
+                  <h3>{document.node.title}</h3>
+                </Carousel.Caption>
+              </Carousel.Item>
+            ))}
+          </Carousel> */}
+        </div>
+        <div>
+          <h2 className='text-2xl font-medium pb-2 mb-4 border-b border-black leading-none'>
+            Latest
+          </h2>
+          <ul>
+            {recentArticles.map(document => (
+              <li key={document.node.id} className="mt-4 pb-4 border-b" style={{ borderBottomColor: '#e2e2e2' }}>
+                <Preview article={document.node} format="small" />
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div>
+          <h2 className='text-2xl font-medium pb-2 mb-4 border-b border-black leading-none'>
+            Labscope
+          </h2>
+          <ul>
+            {labscopeArticles.map(document => (
+              <li key={document.node.id} className="mt-4 pb-4 border-b" style={{ borderBottomColor: '#e2e2e2' }}>
+                <Preview article={document.node} format="small" />
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div>
+          <h2 className='text-2xl font-medium pb-2 mb-4 border-b border-black leading-none'>
+            Noteworthy
+          </h2>
+          <ul>
+            {noteworthyArticles.map(document => (
+              <li key={document.node.id} className="mt-4 pb-4 border-b" style={{ borderBottomColor: '#e2e2e2' }}>
+                <Preview article={document.node} format="small" />
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </Layout>
+  )
+}
+  
+export default IndexPage;
+
+// gql query
+export const splashQuery = graphql`
+  query SplashQuery {
+    allStrapiArticle(
+      sort: { order: DESC, fields: published_at }
+    ) {
+      edges {
+        node {
+          id
+          title
+          author {
+            name
+          }
+          content
+          categories {
+            id
+            title
+          }
+          published_at
+          updatedAt
+        }
+      }
+    }
+  }
+`
+
+// export const splashQuery = graphql`
+//   query SplashQuery {
+//     allStrapiArticle(
+//       sort: { order: DESC, fields: published_at }
+//     ) {
+//       edges {
+//         node {
+//           id
+//           image {
+//             publicURL
+//           }
+//           title
+//           author {
+//             name
+//           }
+//           content
+//           categories {
+//             id
+//             title
+//           }
+//           published_at
+//           updatedAt
+//         }
+//       }
+//     }
+//   }
+// `
---
+++ frontend/src/templates/category.js
@@ -0,0 +1,382 @@
+import React, { useState, useEffect } from 'react'
+import { Link, graphql } from "gatsby"
+import Layout from "../components/layout"
+
+const CategoryTemplate = ({ data }) => {
+  const sortedByDate = data.strapiCategory.articles.sort((a, b) => {
+    let aDate = parseInt(a.published_at.split("T")[0].split("-").join(""))
+    let bDate = parseInt(b.published_at.split("T")[0].split("-").join(""))
+    return (bDate - aDate)
+  })
+
+  const [list, setList] = useState([...sortedByDate.slice(0, 10)])
+  // State to trigger load more
+  const [loadMore, setLoadMore] = useState(false)
+  // State of whether there is more to load
+  const [hasMore, setHasMore] = useState(sortedByDate.length > 10)
+  // Load more button click
+  const handleLoadMore = () => {
+    setLoadMore(true)
+  }
+  // Handle loading more articles
+  useEffect(() => {
+    if (loadMore && hasMore) {
+      const currentLength = list.length
+      const isMore = currentLength < sortedByDate.length
+      const nextResults = isMore
+        ? sortedByDate.slice(currentLength, currentLength + 10)
+        : []
+      setList([...list, ...nextResults])
+      setLoadMore(false)
+    }
+  }, [loadMore, hasMore]) //eslint-disable-line
+  //Check if there is more
+  useEffect(() => {
+    const isMore = list.length < sortedByDate.length
+    setHasMore(isMore)
+  }, [list]) //eslint-disable-line
+  
+  return (
+    <Layout>
+      <div className="">
+        <h2 className="font-normal mb-12 text-4xl leading-tight">{data.strapiCategory.title}</h2>
+        <ul>
+          {list.map(document => (
+            <li key={document.id} className="mb-4">
+              <div className="flex items-start">
+                {/* {document.image ?
+                  <div className="mr-6">
+                    <img src={document.image.publicURL} />
+                  </div>
+                  :
+                  ""
+                } */}
+                <div>
+                  <Link to={`/article/${document.title.split(/[\s!"#$%&'()*+,\-./:;<=>?@[\\\]^_‘{|}~]+/).map((a) => a.toLowerCase()).join("-")}`}>
+                    <h2 className="font-normal mb-4 text-2xl leading-tight">{document.title}</h2>
+                  </Link>
+                  {/* <ReactMarkdown
+                    source={`${document.content.slice(0, 300)}...`}
+                    transformImageUri={uri => uri.startsWith('http') ? uri : `${process.env.IMAGE_BASE_URL}${uri}`}
+                    className="mb-4"
+                  /> */}
+                  {data.allStrapiAuthors.edges.map(author => (
+                    <p className='mb-2 text-base' key={author.node.id}>
+                      {+author.node.id.split("_")[1] === document.author ?
+                        <Link 
+                          className="font-medium underline"
+                          to={`/author/${author.node.name.split(" ").map((a) => a.toLowerCase()).join("-")}`}
+                        >
+                          By {author.node.name}
+                        </Link>
+                        :
+                        ""
+                      }
+                    </p>
+                  ))}
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+        {hasMore ? (
+          <button onClick={handleLoadMore}>Load More</button>
+        ) : (
+          <p>No more results</p>
+        )}
+      </div>
+    </Layout>
+  )
+}
+
+export default CategoryTemplate;
+
+export const query = graphql`
+  query CategoryTemplate($id: String!) {
+    strapiCategory(id: { eq: $id }) {
+      id
+      title
+      articles {
+        id
+        title
+        author
+        content
+        magazine
+        published_at
+      }
+    }
+    allStrapiAuthors {
+      edges {
+        node {
+          id
+          name
+        }
+      }
+    }
+  }
+`
+
+// export const query = graphql`
+//   query CategoryTemplate($id: String!) {
+//     strapiCategory(id: { eq: $id }) {
+//       id
+//       title
+//       articles {
+//         id
+//         title
+//         author
+//         content
+//         magazine
+//         image {
+//           publicURL
+//         }
+//         published_at
+//       }
+//     }
+//     allStrapiAuthors {
+//       edges {
+//         node {
+//           id
+//           name
+//         }
+//       }
+//     }
+//   }
+// `
+
+// /////////////////////////////  INFINTE SCROLLING  ///////////////////////////
+
+// // import React from "react"
+// import React, { useState, useEffect } from 'react'
+// // import { graphql } from "gatsby"
+// import { Link, graphql } from "gatsby"
+// // import Img from "gatsby-image"
+// import Layout from "../components/layout"
+// import ReactMarkdown from "react-markdown"
+// // import Preview from "../components/preview"
+
+// const CategoryTemplate = ({ data }) => {
+//   const sortedByDate = data.strapiCategory.articles.sort((a, b) => {
+//     let aDate = parseInt(a.published_at.split("T")[0].split("-").join(""))
+//     let bDate = parseInt(b.published_at.split("T")[0].split("-").join(""))
+//     return (bDate - aDate)
+//   // }).slice(0, 10)
+//   })
+
+// //   const [ active, setActive ] = useState(0)
+// //   const [ hasMore, setMore ] = useState(edges.length > 10)
+//   // const [ hasMore, setMore ] = useState(data.strapiCategory.articles.length > 10)
+//   const [ hasMore, setMore ] = useState(sortedByDate.length > 10)
+// //   const [ currentList, addToList ] = useState([...edges.slice(0, 10)])
+//   // const [ currentList, addToList ] = useState([...data.strapiCategory.articles.slice(0, 10)])
+//   const [ currentList, addToList ] = useState([...sortedByDate.slice(0, 10)])
+  
+//   const loadEdges = () => {
+//     const currentLength = currentList.length
+//     // const more = currentLength < edges.length
+//     // const more = currentLength < data.strapiCategory.articles.length
+//     const more = currentLength < sortedByDate.length
+//     // const nextEdges = more ? edges.slice(currentLength, currentLength + 20) : []
+//     // const nextEdges = more ? data.strapiCategory.articles.length.slice(currentLength, currentLength + 20) : []
+//     const nextEdges = more ? sortedByDate.slice(currentLength, currentLength + 20) : []
+//     setMore(more)
+//     addToList([...currentList, ...nextEdges])
+//   }
+
+//   const handleScroll = () => {
+//     // if ( !hasMore || isLoading ) return;
+//     if ( !hasMore ) return;
+//     // if ( window.innerHeight + document.documentElement.scrollTop === document.documentElement.offsetHeight ){
+//     if ( window && (
+//        ( window.innerHeight + document.documentElement.scrollTop ) >= document.documentElement.offsetHeight )
+//     ){
+//       loadEdges()
+//     }
+//   }
+
+// //   const handleTouchEnd = (e) => {
+// //     e.preventDefault(); 
+// //     handleScroll();
+// //   }
+//   const handleTouchEnd = (e) => {
+//     if (e.target.tagName !== "a") {
+//       e.preventDefault(); 
+//       handleScroll();
+//     } else {
+//       console.log("this makes me a click event, most likely")
+//     }
+//   }
+
+//   useEffect(() => {
+//     window && window.addEventListener('scroll', handleScroll)
+//     window && window.addEventListener('resize', handleScroll)
+//     window && window.addEventListener('touchend', handleTouchEnd)
+//     return () => {
+//       window && window.removeEventListener('scroll', handleScroll)
+//       window && window.removeEventListener('resize', handleScroll)
+//       window && window.removeEventListener('touchend', handleTouchEnd)
+//     }
+// //   }, [hasMore, currentList])
+//   })
+
+//   return (
+//     <Layout>
+//       <div className="">
+//         <h2 className="font-normal mb-12 text-4xl leading-tight">{data.strapiCategory.title}</h2>
+//         <ul>
+//           {/* {data.strapiCategory.articles.sort((a, b) => b.published_at - a.published_at).slice(0, 10).map(document => ( */}
+//           {sortedByDate.map((document, idx) => (
+//             // <li key={document.id} className="mb-4">
+//             //   <Preview article={document} format="medium" />
+//             // </li>
+//           <li key={document.id} className="mb-4">
+//             <div className="flex items-start">
+//               {/* {document.image ?
+//                 <div className="mr-6">
+//                   <img src={document.image.publicURL} />
+//                 </div>
+//                 :
+//                 ""
+//               } */}
+//               <div>
+//                 <Link to={`/article/${document.title.split(/[\s!"\#$%&'()*+,\-./:;<=>?@\[\\\]^_‘{|}~]+/).map((category) => category.toLowerCase()).join("-")}`}>
+//                   <h2 className="font-normal mb-4 text-2xl leading-tight">{document.title}</h2>
+//                 </Link>
+//                 <ReactMarkdown
+//                   source={`${document.content.slice(0, 300)}...`}
+//                   transformImageUri={uri => uri.startsWith('http') ? uri : `${process.env.IMAGE_BASE_URL}${uri}`}
+//                   className="mb-4"
+//                 />
+//                 {data.allStrapiAuthors.edges.map(author => (
+//                   <p className='mb-2 text-base' key={author.node.id}>
+//                     {+author.node.id.split("_")[1] === document.author ?
+//                       <Link 
+//                         className="font-medium underline"
+//                         to={`/author/${author.node.name.split(" ").map((a) => a.toLowerCase()).join("-")}`}
+//                       >
+//                         By {author.node.name}
+//                       </Link>
+//                       :
+//                       ""
+//                     }
+//                   </p>
+//                 ))}
+//               </div>
+//             </div>
+//           </li>
+//           ))}
+//         </ul>
+//         {
+//           !hasMore &&
+//             <div>All Businesses Loaded!</div>
+//         }
+//         {
+//           hasMore &&
+//             <div>Scroll Down to Load More...</div>
+//         }
+//       </div>  
+//     </Layout>
+//   )
+// }
+
+// export default CategoryTemplate;
+
+// //////////////////////////////  DEFAULT  //////////////////////////////
+
+// // import React from "react"
+// import React, { useState, useEffect } from 'react'
+// // import { graphql } from "gatsby"
+// import { Link, graphql } from "gatsby"
+// // import Img from "gatsby-image"
+// import Layout from "../components/layout"
+// import ReactMarkdown from "react-markdown"
+// // import Preview from "../components/preview"
+
+// const CategoryTemplate = ({ data }) => {
+//   const sortedByDate = data.strapiCategory.articles.sort((a, b) => {
+//     let aDate = parseInt(a.published_at.split("T")[0].split("-").join(""))
+//     let bDate = parseInt(b.published_at.split("T")[0].split("-").join(""))
+//     return (bDate - aDate)
+//   }).slice(0, 10)
+  
+//   return (
+//   <Layout>
+//     <div className="">
+//       <h2 className="font-normal mb-12 text-4xl leading-tight">{data.strapiCategory.title}</h2>
+//     <ul>
+//       {/* {data.strapiCategory.articles.sort((a, b) => b.published_at - a.published_at).slice(0, 10).map(document => ( */}
+//       {sortedByDate.map(document => (
+//         // <li key={document.id} className="mb-4">
+//         //   <Preview article={document} format="medium" />
+//         // </li>
+//       <li key={document.id} className="mb-4">
+//         <div className="flex items-start">
+//           {/* {document.image ?
+//             <div className="mr-6">
+//               <img src={document.image.publicURL} />
+//             </div>
+//             :
+//             ""
+//           } */}
+//           <div>
+//             <Link to={`/article/${document.title.split(/[\s!"\#$%&'()*+,\-./:;<=>?@\[\\\]^_‘{|}~]+/).map((category) => category.toLowerCase()).join("-")}`}>
+//               <h2 className="font-normal mb-4 text-2xl leading-tight">{document.title}</h2>
+//             </Link>
+//             <ReactMarkdown
+//               source={`${document.content.slice(0, 300)}...`}
+//               transformImageUri={uri => uri.startsWith('http') ? uri : `${process.env.IMAGE_BASE_URL}${uri}`}
+//               className="mb-4"
+//             />
+//             {data.allStrapiAuthors.edges.map(author => (
+//               <p className='mb-2 text-base' key={author.node.id}>
+//                 {+author.node.id.split("_")[1] === document.author ?
+//                   <Link 
+//                     className="font-medium underline"
+//                     to={`/author/${author.node.name.split(" ").map((a) => a.toLowerCase()).join("-")}`}
+//                   >
+//                     By {author.node.name}
+//                   </Link>
+//                   :
+//                   ""
+//                 }
+//               </p>
+//             ))}
+//           </div>
+//         </div>
+//       </li>
+//       ))}
+//     </ul>
+//     </div>
+//   </Layout>
+//   )
+// }
+
+// export default CategoryTemplate;
+
+// export const query = graphql`
+//   query CategoryTemplate($id: String!) {
+//     strapiCategory(id: { eq: $id }) {
+//       id
+//       title
+//       articles {
+//         id
+//         title
+//         author
+//         content
+//         magazine
+//         image {
+//           publicURL
+//         }
+//         published_at
+//       }
+//     }
+//     allStrapiAuthors {
+//       edges {
+//         node {
+//           id
+//           name
+//         }
+//       }
+//     }
+//   }
+// `
