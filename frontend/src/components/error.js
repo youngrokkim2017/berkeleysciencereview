d7b9f60551a34132f7415d8c85c2ed83d309f8df
Kamran Ahmed <k.ahmed@berkeley.edu>
2021-02-19 18:01:35 -0500

    Add error, update mailchimp components, fix article prose

---
+++ frontend/src/components/error.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import ReactMarkdown from "react-markdown"
+
+const Error = ({ msg }) => {
+
+    return (
+        <div className="fixed z-50 inset-0 overflow-y-auto sans-serif">
+            <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+                <div className="fixed inset-0 transition-opacity" aria-hidden="true">
+                    <div className="absolute inset-0 bg-gray-500 opacity-50"></div>
+                </div>
+                <span className="hidden sm:inline-block sm:align-middle sm:h-screen" aria-hidden="true">&#8203;</span>
+                <div className="inline-block align-bottom bg-white rounded-md text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-lg sm:w-full" role="dialog" aria-modal="true" aria-labelledby="modal-headline">
+                    <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+                        <div className="sm:flex sm:items-start">
+                            <div className="mx-auto flex-shrink-0 flex items-center justify-center h-12 w-12 rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                                <svg className="h-6 w-6 text-red-600" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
+                                </svg>
+                            </div>
+                            <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
+                                <h3 className="text-lg leading-6 font-medium text-black" id="modal-headline">
+                                    Error
+                                </h3>
+                                <div className="mt-2">
+                                {/* don;t know how to pass links when signing up with repeated email */}
+                                    <p className="text-sm text-gray-500">
+                                        {/* <ReactMarkdown
+                                            source={msg}
+                                        /> */}
+                                        {msg}
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="bg-gray-50 px-4 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+                        <button type="button" className="mt-3 w-full inline-flex justify-center rounded-sm border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
+                            Close
+                        </button>
+                        {/* add close functionality */}
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Error
--- frontend/src/components/mailchimpHome.js
+++ frontend/src/components/mailchimpHome.js
@@ -2,14 +2,14 @@ import React from 'react'
 import addToMailchimp from 'gatsby-plugin-mailchimp'
 import Error from './error.js'
 
-class MailchimpComponent extends React.Component {
+class MailchimpComponentHome extends React.Component {
     state = {
         email: null,
         processing: false,
         success: false,
-        error : ''
+        error: ''
     }
-    
+
     _handleChange = e => {
         // console.log({
         //     [`${e.target.name}`]: e.target.value,
@@ -25,7 +25,6 @@ class MailchimpComponent extends React.Component {
         // console.log('submit', this.state)
 
         // addToMailchimp(this.state.email, this.state)
-
         this.setState({ processing: true });
         addToMailchimp(this.state.email)
             .then(({ msg, result }) => {
@@ -46,20 +45,18 @@ class MailchimpComponent extends React.Component {
                 {this.state.error ? <Error msg={this.state.error} /> : ''}
                 <h2 className="font-bold mb-1">Sign up for the BSR Newsletter</h2>
                 <div>
-                    <p className="mb-4">Get an overview of our latest news, along with fascinating images and people. Sent about once a month.</p>
-
-                    <form onSubmit={this._handleSubmit} className={`bg-gray-100 text-gray-600 flex items-center py-2 px-4 pr-2 border focus-within:border-blue-600 rounded ${this.state.success ? 'hidden' : 'block'}`}>
+                    <p className="mb-4">Get an overview of our latest news, along with fascinating images and people.</p>
+                    <form onSubmit={this._handleSubmit} className={`text-gray-600 ${this.state.success ? 'hidden' : 'block'}`}>
                         <input
                             type="email"
                             onChange={this._handleChange}
                             placeholder="Email address"
                             name="email"
-                            className="bg-transparent border-none w-full text-black placeholder-gray-600 leading-tight focus:outline-none mr-2"
+                            className="rounded outline-none w-full text-black placeholder-gray-600 leading-tight mr-2 py-2 px-4 pr-2 mb-4 border border-gray-400 focus-within:border-blue-600"
                         />
-
                         {this.state.processing
                             ?
-                            <span className="inline-flex px-4 py-2 leading-none text-white flex-shrink-0 bg-gray-500 transition ease-in-out duration-150 cursor-not-allowed rounded">
+                            <span className="inline-flex w-full px-4 py-3 leading-none text-white flex-shrink-0 bg-gray-500 transition ease-in-out duration-150 cursor-not-allowed rounded">
                                 <svg className="animate-spin -ml-1 mr-3 h-4 w-4 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
                                     <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
                                     <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
@@ -67,10 +64,9 @@ class MailchimpComponent extends React.Component {
                                 Processing
                             </span>
                             :
-                            <input type="submit" className="inline-block px-4 py-2 leading-none text-white flex-shrink-0 cursor-pointer bg-black rounded" value="Subscribe" /> 
+                            <input type="submit" className="w-full px-4 py-3 leading-none text-white flex-shrink-0 cursor-pointer bg-black rounded" value="Subscribe" />
                         }
                     </form>
-
                     <div className={this.state.success ? 'block' : 'hidden'}>
                         Thank you for subscribing! Please check your email for confirmation.
                     </div>
@@ -80,4 +76,50 @@ class MailchimpComponent extends React.Component {
     }
 }
 
-export default MailchimpComponent;
+export default MailchimpComponentHome;
+
+// class MailchimpComponent extends React.Component {
+//   // Since `addToMailchimp` returns a promise, you
+//   // can handle the response in two different ways:
+
+//   // Note that you need to send an email & optionally, listFields
+//   // these values can be pulled from React state, form fields,
+//   // or wherever.  (Personally, I recommend storing in state).
+
+//   // 1. via `.then`
+//   _handleSubmit = e => {
+//     e.preventDefault();
+//     // addToMailchimp(email, listFields) // listFields are optional if you are only capturing the email address.
+//     addToMailchimp(email) 
+//     .then(data => {
+//       // I recommend setting data to React state
+//       // but you can do whatever you want (including ignoring this `then()` altogether)
+//       console.log(data)
+//     })
+//     .catch(() => {
+//       // unnecessary because Mailchimp only ever
+//       // returns a 200 status code
+//       // see below for how to handle errors
+//     })
+//   }
+
+//   // 2. via `async/await`
+//   _handleSubmit = async (e) => {
+//     e.preventDefault();
+//     // const result = await addToMailchimp(email, listFields)
+//     const result = await addToMailchimp(email)
+//     // I recommend setting `result` to React state
+//     // but you can do whatever you want
+//   }
+
+//   render () {
+//     return (
+//       // <form onSubmit={this._handleSubmit(email, {listFields})}>
+//       <form onSubmit={this._handleSubmit(email)}>
+
+//       </form>
+//     )
+//   }
+// }
+
+// export default MailchimpComponent;
