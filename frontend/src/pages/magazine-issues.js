43cdca38b77f3d304924137f31c31f0ff9f12c57
Patrick <patrick.yrkim@gmail.com>
2021-02-20 21:14:43 -0800

    Add magazine index page

---
+++ frontend/src/pages/magazine-issues.js
@@ -0,0 +1,40 @@
+import React from "react"
+import { Link } from "gatsby"
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const MagazineIssuePage = ({ data }) => {
+  const magazinesSortedByIssue = data.allStrapiMagazineIssue.edges.sort((a, b) => b.node.issue - a.node.issue);
+  return (
+    <Layout>
+      <SEO title="Magazine issues" />
+      <div>
+        {magazinesSortedByIssue.map(document => (
+          <div>
+            <Link to={`/magazine/${document.node.title.split(" ").map((a) => a.toLowerCase()).join("-")}`}>
+              {document.node.title}
+            </Link>
+          </div>
+        ))}
+      </div>
+    </Layout>
+  )
+}
+
+export default MagazineIssuePage;
+
+export const magazineIssuePageQuery = graphql`
+  query MagazineIssuePageQuery {
+    allStrapiMagazineIssue {
+      edges {
+        node {
+          id
+          title
+          pdf {
+            publicURL
+          }
+        }
+      }
+    }
+  }
+`
